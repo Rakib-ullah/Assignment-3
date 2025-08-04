cf079741d0b973b8cdf884b33402333727422d48
Rakib ullah <rakibullah19730@gmail.com>
2025-08-04 13:22:09 +0600

    Final task update

--- poblem3.js
+++ poblem3.js
@@ -1,5 +1,5 @@
 /** Problem -03 ( Medicine Planner ) */
-var lastDay = 7 ;
+var lastDay = 20 ;
 //write your code here
 
 for ( var day = 1; day <= lastDay; day++){
--- poblem5.js
+++ poblem5.js
@@ -1,5 +1,5 @@
 /** Problem 05 - ( PH Email Generator )  */
-var student= { name: "jhankar" , roll: 1014 ,department: "cse" };
+var student= { name: "monu" , roll: 99,department: "eee"  };
 //write your code here
 var email = student.name + student.roll + "." + student.department + "@ph.ac.bd";
 console.log(email)
